da979de76f5c846caaaca00528a50dfebaafbc77
Babu Epaphras <babuepaphras@gmail.com>
2016-04-21 14:34:04 +0530

    Added files via upload

---
+++ acceleratorstorefrontcommons/commonweb/src/de/hybris/platform/acceleratorstorefrontcommons/forms/PostAddForm.java
@@ -0,0 +1,145 @@
+/**
+ *
+ */
+package de.hybris.platform.acceleratorstorefrontcommons.forms;
+
+/**
+ * @author admin
+ *
+ */
+public class PostAddForm
+{
+	/* private CatalogVersionModel catalogVersion; */
+	private String code;
+
+	private String typeofadd;
+	private String adtitle;
+	private String description;
+	private String catgory;
+	private String valid;
+	private Integer phno;
+
+
+
+
+	/**
+	 * @return the code
+	 */
+	public String getCode()
+	{
+		return code;
+	}
+
+	/**
+	 * @param code
+	 *           the code to set
+	 */
+	public void setCode(final String code)
+	{
+		this.code = code;
+	}
+
+
+	/**
+	 * @return the typeofadd
+	 */
+	public String getTypeofadd()
+	{
+		return typeofadd;
+	}
+
+	/**
+	 * @param typeofadd
+	 *           the typeofadd to set
+	 */
+	public void setTypeofadd(final String typeofadd)
+	{
+		this.typeofadd = typeofadd;
+	}
+
+	/**
+	 * @return the adtitle
+	 */
+	public String getAdtitle()
+	{
+		return adtitle;
+	}
+
+	/**
+	 * @param adtitle
+	 *           the adtitle to set
+	 */
+	public void setAdtitle(final String adtitle)
+	{
+		this.adtitle = adtitle;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription()
+	{
+		return description;
+	}
+
+	/**
+	 * @param description
+	 *           the description to set
+	 */
+	public void setDescription(final String description)
+	{
+		this.description = description;
+	}
+
+	/**
+	 * @return the catgory
+	 */
+	public String getCatgory()
+	{
+		return catgory;
+	}
+
+	/**
+	 * @param catgory
+	 *           the catgory to set
+	 */
+	public void setCatgory(final String catgory)
+	{
+		this.catgory = catgory;
+	}
+
+	/**
+	 * @return the valid
+	 */
+	public String getValid()
+	{
+		return valid;
+	}
+
+	/**
+	 * @param valid
+	 *           the valid to set
+	 */
+	public void setValid(final String valid)
+	{
+		this.valid = valid;
+	}
+
+	/**
+	 * @return the phno
+	 */
+	public Integer getPhno()
+	{
+		return phno;
+	}
+
+	/**
+	 * @param phno
+	 *           the phno to set
+	 */
+	public void setPhno(final Integer phno)
+	{
+		this.phno = phno;
+	}
+
+}
